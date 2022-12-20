6a7a147fe2b00670ba10d9fa6d765fa8eb857388
raymond6066 <adedipe6@gmail.com>
2022-12-20 10:57:28 -0800

    joy

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,8 +39,8 @@ class Square:
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
-           raise TypeError("position must be a tuple of 2 positive integers")
-       self.__position = value
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Return the current area of the square."""
@@ -51,7 +51,6 @@ class Square:
         if self.__size == 0:
             print("")
             return
-
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
